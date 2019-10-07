1f2f1a0df6e8250db58a953dcff61a114f2698e7
ifeelok92 <ifeelok@sina.com>
2019-10-07 14:51:07 +0800

    Update most thing

--- main.c
+++ main.c
@@ -11,42 +11,46 @@ int main() {
     unsigned char sk[PKE_SECRETKEYBYTES];
     unsigned long long pklen;
     unsigned long long sklen;
-    unsigned char m[128];
+    unsigned char m[128] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam est, vestibulum ut vehicula non, tincidunt non odio. Aenean";
     unsigned char c[2048];
-    unsigned long long mlen;
-    unsigned long long clen;
+    unsigned long long mlen = PKE_MESS_BYTES;
+    unsigned long long clen = PKE_CIP_BYTES;
 
-    clock_t start, finish;
-    double total_time;
+    // clock_t start, finish;
+    // double total_time;
 
 
-    start = clock();
-    for (int i = 0; i < NTESTS; i++) {
-        pke_keygen(pk, &pklen, sk, &sklen);
-    }
-    finish = clock();
-    total_time = (double)(finish - start) / CLOCKS_PER_SEC;
-    printf("keygen %f us\n", (total_time * 1000000 / NTESTS));
+    // start = clock();
+    // for (int i = 0; i < NTESTS; i++) {
+    //     pke_keygen(pk, &pklen, sk, &sklen);
+    // }
+    // finish = clock();
+    // total_time = (double)(finish - start) / CLOCKS_PER_SEC;
+    // printf("keygen %f us\n", (total_time * 1000000 / NTESTS));
     
-    pke_keygen(pk, &pklen, sk, &sklen);
-    start = clock();
-    for (int i = 0; i < NTESTS; i++) {
-        pke_enc(pk, PKE_PUBLICKEYBYTES, m, PKE_MESS_BYTES, c, &clen);
-    }
-    finish = clock();
-    total_time = (double)(finish - start) / CLOCKS_PER_SEC;
-    printf("enc %f us\n", (total_time * 1000000 / NTESTS));
+    // pke_keygen(pk, &pklen, sk, &sklen);
+    // start = clock();
+    // for (int i = 0; i < NTESTS; i++) {
+    //     pke_enc(pk, PKE_PUBLICKEYBYTES, m, PKE_MESS_BYTES, c, &clen);
+    // }
+    // finish = clock();
+    // total_time = (double)(finish - start) / CLOCKS_PER_SEC;
+    // printf("enc %f us\n", (total_time * 1000000 / NTESTS));
+
+    // pke_keygen(pk, &pklen, sk, &sklen);
+    // pke_enc(pk, PKE_PUBLICKEYBYTES, m, PKE_MESS_BYTES, c, &clen);
+    // start = clock();
+    // for (int i = 0; i < NTESTS; i++) {
+    //     pke_dec(sk, PKE_SECRETKEYBYTES, c, clen, m, &mlen);
+    // }
+    // finish = clock();
+    // total_time = (double)(finish - start) / CLOCKS_PER_SEC;
+    // printf("dec %f us\n", (total_time * 1000000 / NTESTS));
 
     pke_keygen(pk, &pklen, sk, &sklen);
-    pke_enc(pk, PKE_PUBLICKEYBYTES, m, PKE_MESS_BYTES, c, &clen);
-    start = clock();
-    for (int i = 0; i < NTESTS; i++) {
-        pke_dec(sk, PKE_SECRETKEYBYTES, c, clen, m, &mlen);
-    }
-    finish = clock();
-    total_time = (double)(finish - start) / CLOCKS_PER_SEC;
-    printf("dec %f us\n", (total_time * 1000000 / NTESTS));
-
+    pke_enc(pk, pklen, m, PKE_MESS_BYTES, c, &clen);
+    pke_dec(sk, sklen, c, clen, m, &mlen);
+    printf("%s\n", m);
 
     return 0;
 }
